5101207d2ab28a07b2774067f04947ef73f5a403
Islam Salah <46622354+islamsalah2020@users.noreply.github.com>
2021-09-19 03:09:43 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region  = var.region
   #profile = "default"
   #version = "2.30.0"
-  access_key = "AKIA5TVFZX2ZGR25D37E"
-  secret_key = "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"
+  access_key = "my_access_key"
+  secret_key = "my_secret_key"
 }
